49a3d03336a1dc61048c1656da4c677aa71c0e9d
ugurcanonal10-ctrl <ugurcanonalosmankursad10@gmail.com>
2026-06-23 14:03:00 +0300

    Fix import statement for PerceptionPipeline

--- atlas_planning/main.py
+++ atlas_planning/main.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-from .perception import PerceptionPipeline
+from perception import PerceptionPipeline
 
 class RobotaksiPerceptionNode(Node):
     def __init__(self):
